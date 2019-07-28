62a57a092e82a4b64a7f2562639f169861e2ced6
A Usoro <aolausoro@gmail.com>
2019-07-28 02:37:24 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, {useEffect, Fragment} from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/Techs/AddTechModal';
+import TechListModal from './components/Techs/TechListModal';
+import './App.css';
+
+const App = () => {
+  useEffect(() => {
+    //Init Materialize JS
+    M.AutoInit();
+  })
+
+  return (
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ src/reducers/techReducer.js
@@ -0,0 +1,43 @@
+import { GET_TECHS, ADD_TECH, SET_LOADING, DELETE_TECH, TECHS_ERROR } from '../actions/Types'
+const initialState = {
+    techs: null,
+    loading: false,
+    error: null
+}
+
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case GET_TECHS:
+            return {
+                ...state,
+                techs: action.payload,
+                loading: false
+            }
+        case ADD_TECH:
+            return {
+                ...state,
+                techs: [...state.techs, action.payload],
+                loading: false
+            }
+        case TECHS_ERROR:
+            return {
+                ...state,
+                error: action.payload,
+                loading: false
+            }
+        case SET_LOADING:
+            return {
+                ...state,
+                loading: true
+            }
+        case DELETE_TECH:
+            return {
+                ...state,
+                techs: state.techs.filter(tech => tech.id !== action.payload),
+                loading: false
+            }
+        default:
+            return state;
+    }
+};
